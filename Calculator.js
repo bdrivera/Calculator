edee83aff5a09a7a473fa1749fb315e1fc7a428f
Bret Rivera <bdrrivera@gmail.com>
2020-07-27 12:39:45 -0700

    Initial commit Added HTML, CSS, and Javascript base files. Some basic functionality included from previous projects including: -query selector function -useful global variables for later -framework for javascript side CSS

---
+++ Calculator.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/* Global Variable Block */
+const cont = $('#');
+const butt = $('#');
+
+/* Runtime Start*/
+
+/* Runtime End */
+
+
+/**
+ * Establishes basic CSS for Calculator
+ */
+function setUpCSS() {
+    //CSS for board
+    !.style.display = "flex";
+    !.style.flexWrap = "wrap";
+    !.style.margin = "0 auto";
+    !.style.textAlign = "center";
+    !.style.width = etchContWidth + "px";
+    !.style.height = etchContWidth + "px";
+}
+
+/**
+ * Function that queries an inquiry and returns the element.
+ * @param {*} n Element name  to be queried and returned
+ */
+function $(n) { return document.querySelector(n); }
---
+++ README.md
@@ -0,0 +1,4 @@
+Calculator is an HTML, CSS, and Javascript project created by Bret Rivera during The Odin Project curriculum. My goal for this assignment was to meet the main goals of the assignment while adding extra functionality to show growth over the course of the project.
+
+The goal of the assignment is as such: Create a webpage with a graphic user interface calculator capable of addition, subtraction, multiplication, and division. It should also be capable of displaying functions and input in real time. The calculator will also include quality of life features to include rounding of long decimals and protection from invalid inputs. 
+
